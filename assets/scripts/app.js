2d2e5586528c5ffe958abc677bb4fcbcdf5a22b5
MacBook Pro5 <macbookpro5@MacBooks-MacBook-Pro-99.local>
2018-10-03 15:47:44 -0400

    Previous work
    before deployment

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,7 +17,6 @@ $(() => {
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
   $('#sign-out-button').on('click', authEvents.onSignOut)
-  // $('#create-example-form').on('submit', gameClientEvents.onCreate)
   $('#new-game').on('click', authEvents.onNewGame)
   $('#square').on('click', gameClientEvents.onPlayGame)
 })
